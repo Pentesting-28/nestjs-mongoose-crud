9aa93877b796b44146bc78ff540a621c4d741750
Pentesting-28 <mpenav28@gmail.com>
2023-01-28 19:54:59 -0400

    Config conex mongodb to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,15 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [UsersModule, ProductsModule],
+  
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/crud_api_db'),
+    UsersModule,
+    ProductsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
