2746fda89b7e2a19ec4991c0c8b8c79406fda668
Pentesting-28 <mpenav28@gmail.com>
2023-01-29 17:10:57 -0400

    Create schema Model injection user.schema.ts

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>
+
+@Schema()
+export class User {
+
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ required: true })
+    last_name: string;
+
+    @Prop({ required: true, unique: true, lowercase: true })
+    email: string
+
+    @Prop({ required: true })
+    password: string
+
+    @Prop({ default: Date.now() })
+    created_date: Date
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
