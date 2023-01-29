bbcd0b7c0dc32ff7676c65cbdcdf4c160f47260f
Pentesting-28 <mpenav28@gmail.com>
2023-01-29 14:17:04 -0400

    implement class-validator in data transfer object

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,25 @@
-export class CreateProductDto {}
+import { 
+    IsString,
+    MinLength,
+    MaxLength,
+    IsNotEmpty
+} from 'class-validator';
+
+export class CreateProductDto {
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(255)
+    name: string
+    
+    @IsString()
+    @IsNotEmpty()
+    price: string
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(599)
+    description: string
+}
