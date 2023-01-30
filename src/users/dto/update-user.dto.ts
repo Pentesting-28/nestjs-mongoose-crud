6b70c3a3977de30aa45a40c63efc7889dd231eec
Pentesting-28 <mpenav28@gmail.com>
2023-01-30 16:32:06 -0400

    Modifying and adjusting the validation logic of update-user.dto.ts

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,27 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateUserDto } from './create-user.dto';
+import { 
+    IsString,
+    IsEmail,
+    MinLength,
+    MaxLength,
+    IsNotEmpty
+} from 'class-validator';
+// import { PartialType } from '@nestjs/swagger';
+// import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto /*extends PartialType(CreateUserDto)*/ {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(255)
+    name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(255)
+    last_name: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+}
